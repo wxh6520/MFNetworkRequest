a379f5178baf695e1a425ffb8c137c82f753cd7d
wxh6520 <wxh@eeepay.cn>
2017-02-14 13:03:07 +0800

    使用MFNetworkRequest示例

--- MFNetworkRequest_Swift/ViewController.swift
+++ MFNetworkRequest_Swift/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text: UITextView!
+    
+    @IBAction func send(_ sender: Any) {
+        
+        let login = MFNetworkRequest()
+        login.sendDataGetRequest(url: "https://www.baidu.com", successBlock: { (successStr) in
+            self.text.text = successStr
+        }) { (failStr) in
+            self.text.text = failStr
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
